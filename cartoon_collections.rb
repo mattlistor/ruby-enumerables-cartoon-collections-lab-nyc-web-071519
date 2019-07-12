7f36e11bdd6fd453aa3aa63cc41b67fbd6350fcf
Matthew Listor <mattlistor@gmail.com>
2019-07-12 00:27:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,5 +12,5 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.map {|i| cheese_types.include(i)}
+  array[array.map {|i| cheese_types.include(i)}]
 end
